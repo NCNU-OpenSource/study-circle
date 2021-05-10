5676f71d939048b4f2202b21396feb626f238b6d
Jerry Wang <focaaby@gmail.com>
2021-05-10 23:42:22 +0100

    Update the record

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,67 @@
 module.exports = {
-  someSidebar: {
-    導讀摘要: ['readme', 'book'],
+  docs: {
+    "導讀摘要": ['readme', 'book'],
+    "鳥哥的 Linux 私房菜：基礎學習篇": [
+      {
+        type: 'doc',
+        id: '2020-11-07',
+        label: '2020-11-07 Ch2, Ch4, Ch5'
+      },
+      {
+        type: 'doc',
+        id: '2020-11-07',
+        label: '2020-11-07 Ch2, Ch4, Ch5'
+      },
+      {
+        type: 'doc',
+        id: '2020-11-21',
+        label: '2020-11-21 Ch6~8'
+      },
+      {
+        type: 'doc',
+        id: '2020-12-05',
+        label: '2020-12-05 Ch9, Ch11'
+      },
+      {
+        type: 'doc',
+        id: '2020-12-19',
+        label: '2020-12-19 Ch10, Ch12'
+      },
+      {
+        type: 'doc',
+        id: '2021-01-09',
+        label: '2021-01-09 Ch14, Ch15'
+      },
+      {
+        type: 'doc',
+        id: '2021-01-23',
+        label: '2021-01-23 Ch16'
+      },
+      {
+        type: 'doc',
+        id: '2021-02-06',
+        label: '2021-02-06 Ch17, Ch19'
+      },
+      {
+        type: 'doc',
+        id: '2021-03-06',
+        label: '2021-03-06 Ch13'
+      },
+      {
+        type: 'doc',
+        id: '2021-03-20',
+        label: '2021-03-20 Ch20'
+      },
+      {
+        type: 'doc',
+        id: '2021-04-10',
+        label: '2021-04-10 Ch18'
+      },
+      {
+        type: 'doc',
+        id: '2021-04-24',
+        label: '2021-04-24 Ch21'
+      }
+    ],
   },
 };
